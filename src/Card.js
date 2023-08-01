46d5f2d4ad92a3fcc0e76db03ac3685ff827331e
Mahesh <dhungelmahesh7@gmail.com>
2023-08-02 01:18:39 +0530

    Added Transistion

--- src/Card.js
+++ src/Card.js
@@ -12,7 +12,9 @@ const Card = ({title, icon, index, subtitle}) => {
     <div onMouseEnter = {() => setHovered(true)} onMouseLeave = {() => setHovered(false)}className={index % 2 === 0 ? "cards gray" : "cards"}>
         <img className='icons' src={icon}></img>
         <p className='title'>{title}</p>
-       {hovered && <p className='subtitle'>{subtitle}</p> }
+
+        <p className = "subtitle" style={{opacity : hovered ? "1" : 0}}>{subtitle}</p>
+
 
     </div>
   )
--- src/ClickdeeInfo.js
+++ src/ClickdeeInfo.js
@@ -11,28 +11,30 @@ import Quality from "./Constants/Svg/Quality.svg"
 export const ClickdeeInfo = [
     {
         icon : Target , 
-        title : "Choose Your Local Targetting",
-        subtitle : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab culpa cupiditate cum, fuga exercitationem vitae recusandae unde 1"
+        title : "Choose Your Local Targeting",
+        subtitle : "Our targeted and tracked calls are tailored to your business needs, audience, and geolocation."
     },
     {
         icon : Conversion , 
         title : "Track Your Conversion ", 
-        subtitle : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab culpa cupiditate cum, fuga exercitationem vitae recusandae unde "
+        subtitle : "We use the most progressive tracking and analytics technology to ensure that every call can be tracked and evaluated"
     },
     {
         icon : Campaigns , 
         title : "Customized Campaigns", 
-        subtitle : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab culpa cupiditate cum, fuga exercitationem vitae recusandae unde "
+        subtitle : "Audiences, budgets, and goals: you decide the criteria for your digital ad campaign, and we’ll do the rest."
+
     },
     {
         icon : User, 
         title : "Get Dedicated Support Team", 
-        subtitle : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab culpa cupiditate cum, fuga exercitationem vitae recusandae unde "
+        subtitle : "We constantly monitor quality control! Our affiliate network of publishers are digital ad experts that have been vetted."
+
     },
     {
         icon : Quality , 
         title : "Quality Assurance", 
-        subtitle : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab culpa cupiditate cum, fuga exercitationem vitae recusandae unde "
+        subtitle : "We constantly monitor quality control! Our affiliate network of publishers are digital ad experts that have been vetted."
     }, 
     
 ]
